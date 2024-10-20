7fa9aa23bc0763caa87da8fdfc963bb89c34325c
nastiknaidenova <nastiknaidenova@yandex.ru>
2024-10-20 11:46:55 +0800

    fix workflow, add secret

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,8 +1,8 @@
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TUser } from './types';
 
-const URL =
-  process.env.BURGER_API_URL || 'https://norma.nomoreparties.space/api';
+const URL = process.env.BURGER_API_URL;
+// process.env.BURGER_API_URL || 'https://norma.nomoreparties.space/api';
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
